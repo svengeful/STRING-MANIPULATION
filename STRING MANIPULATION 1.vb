772d9db2cd95e5e060d7091bd90db42cc7a79723
svengeful <55344552+svengeful@users.noreply.github.com>
2019-10-04 02:31:39 +0500

    REPLACE CHARACTER IN PSEUDO CODE & VISUAL BASIC
    The following pseudo code finds input as string and replaces it as a character in the output.

---
+++ STRING MANIPULATION 1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2, Str3, firstWord, lastWord As String
+        Dim Sp As Integer
+
+        Str1 = ""
+        Str2 = ""
+        Str3 = ""
+        firstWord = ""
+        lastWord = ""
+        Sp = 0
+
+        Console.Write("Enter 1st string of 3 words: ")
+        Str1 = Console.ReadLine
+
+        Console.Write("Enter 2nd string of 3 words: ")
+        Str2 = Console.ReadLine
+
+        Sp = InStr(Str1, " ")
+        firstWord = Right(Str1, Sp - 2)
+
+        Sp = InStr(Str2, " ")
+        lastWord = Left(Str2, Sp - 1)
+
+        Str3 = firstWord & " " & lastWord
+        Console.WriteLine(Str3)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ STRING MANIPULATION 2.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2, Char1, Char2, nextChar As String
+        Dim counter As Integer
+
+        Str1 = ""
+        Str2 = ""
+        Char1 = ""
+        Char2 = ""
+        nextChar = ""
+        counter = 0
+
+        Console.Write("Enter String: ")
+        Str1 = Console.ReadLine
+
+        Console.Write("Enter character to replace: ")
+        Char1 = Console.ReadLine
+
+        Console.Write("Enter character to remove: ")
+        Char2 = Console.ReadLine
+
+        For counter = 1 To Len(Str1)
+            nextChar = Left(Str1, 4)
+            If nextChar = Char1 Then
+                nextChar = Char2
+            End If
+            Str2 = Str2 & nextChar
+        Next
+
+        Console.WriteLine("Final string = " & Str2)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
