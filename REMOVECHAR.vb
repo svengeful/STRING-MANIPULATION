e19710df7b5a34a9fff0bc08051bcf6ea70031bd
svengeful <55344552+svengeful@users.noreply.github.com>
2019-10-14 14:56:16 +0500

    REMOVECHAR
    THIS PROGRAM REMOVES CHARACTER FROM A STRING

---
+++ REMOVECHAR.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mySTR, finalStr, myCHAR, nextchar As String
+        Dim COUNTER As Integer
+
+        mySTR = ""
+        finalStr = ""
+        myCHAR = ""
+        nextchar = ""
+        COUNTER = 0
+
+        Console.WriteLine("enter string : ")
+        mySTR = Console.ReadLine
+
+        Console.Write("enter character to remove: ")
+        myCHAR = Console.ReadLine
+
+        For COUNTER = 1 To Len(mySTR)
+            nextchar = Left(mySTR, 1)
+            If nextchar <> myCHAR Then
+                finalStr = finalStr & nextchar
+            End If
+        Next
+
+        Console.WriteLine(" FINAL STRING is = " & finalStr)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
